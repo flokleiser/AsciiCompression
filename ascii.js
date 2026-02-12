d3edc2b458281958f744aec2599fbe341f177cc4
Flo Kleiser <floriankleiser@gmail.com>
2026-02-12 12:57:10 +0100

    proper image resizing

--- ascii.js
+++ ascii.js
@@ -4,37 +4,69 @@ let useColor = true
 let scaleFactor = 8
 const density = "Ñ@#W$9876543210?!abc;:+=-,._ "
 
+let originalImg
+
 function preload() {
-    img = loadImage("../assets/toni.jpg")
+    originalImg = loadImage("../assets/toni.jpg")
 }
 
 function setup() {
     noCanvas()
+
+    img = originalImg.get()
+    img.loadPixels()
+
+    const fontSize = 8
+    const lineHeight = 0.8
+    const targetHeight = windowHeight * 0.95
+
+    const renderedCharHeight = fontSize * lineHeight
+    const neededImageHeight = targetHeight * scaleFactor / renderedCharHeight
+
+    const ratio = neededImageHeight / img.height
+    img.resize(floor(img.width * ratio), floor(img.height * ratio))
+
     asciiDiv = createDiv()
     asciiDiv.style("font-family", "monospace")
     asciiDiv.style("white-space", "pre")
-    asciiDiv.style("line-height", "0.8em")
-    asciiDiv.style("font-size", "8px")
+    asciiDiv.style("line-height", lineHeight + "em")
+    asciiDiv.style("font-size", fontSize + "px")
+
+    document.body.style.margin = "0"
+    document.body.style.display = "flex"
+    document.body.style.justifyContent = "center"
+    document.body.style.alignItems = "center"
+    document.body.style.height = "100vh"
+    document.body.style.background = "black"
+
     convertImage()
 }
+
 
 function keyPressed() {
     if (key === "c" || key === "C") {
         useColor = !useColor
         convertImage()
     }
-    if (key === "+") {
+    if (keyCode === UP_ARROW) {
         scaleFactor = max(2, scaleFactor - 1)
         convertImage()
+        console.log("up")
     }
-    if (key === "-") {
+
+    if (keyCode === DOWN_ARROW) {
         scaleFactor++
         convertImage()
+        console.log("down")
     }
 }
 
+function windowResized() {
+    setup()
+}
+
 function convertImage() {
-    img.loadPixels()
+    // img.loadPixels()
     let asciiStr = ""
 
     for (let y = 0; y < img.height; y += scaleFactor) {
@@ -60,10 +92,3 @@ function convertImage() {
     asciiDiv.html(asciiStr)
 }
 
-// function keyTyped() {
-//     if (key === 's') {
-//         saveCanvas('myCanvas', 'jpg');
-//         print("saving image");
-//     }
-//     return false;
-// }
